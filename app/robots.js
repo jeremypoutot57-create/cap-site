ab032679c5c9e28c719b6d8716c075d415c115ec
jeremypoutot57-create <jeremy.poutot57@gmail.com>
2026-09-15 15:51:02 +0200

    Delete app directory

--- app/robots.js
+++
@@ -1,7 +0,0 @@
-const SITE = process.env.NEXT_PUBLIC_SITE_URL || "https://cap.arras-patrimoine.fr";
-export default function robots() {
-  return {
-    rules: [{ userAgent: "*", allow: "/", disallow: ["/api/", "/confirmation", "/vsl"] }],
-    sitemap: SITE + "/sitemap.xml",
-  };
-}
--- app/sitemap.js
+++
@@ -1,4 +0,0 @@
-const SITE = process.env.NEXT_PUBLIC_SITE_URL || "https://cap.arras-patrimoine.fr";
-export default function sitemap() {
-  return [{ url: SITE + "/", lastModified: new Date(), changeFrequency: "monthly", priority: 1 }];
-}
